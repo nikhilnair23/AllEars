5df9005663e52eb19ef89073625d17e26eb2b17a
nikhilnair23 <nikhilnair238@gmail.com>
2020-11-24 01:01:23 -0500

    Added buttons and layout

---
+++ app/src/main/java/com/example/allears/IntervalActivity.java
@@ -0,0 +1,7 @@
+package com.example.allears;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntervalActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/allears/MainActivity.java
+++ app/src/main/java/com/example/allears/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.allears;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.chord_training_button:
+                break;
+
+            case R.id.interval_training_button:
+                break;
+        }
     }
 }
