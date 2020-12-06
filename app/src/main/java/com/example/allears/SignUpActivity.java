6685751cbaf73b2ba7eb5c1c59482889c1258a35
nikhilnair23 <nikhilnair238@gmail.com>
2020-12-06 01:52:10 -0500

    Added firebase dependencies

--- app/src/main/java/com/example/allears/SignUpActivity.java
+++ app/src/main/java/com/example/allears/SignUpActivity.java
@@ -1,22 +1,14 @@
 package com.example.allears;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void onClick(View view){
-        switch (view.getId()){
-
-        }
+        setContentView(R.layout.activity_signup);
     }
 }
