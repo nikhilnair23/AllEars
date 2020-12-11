0082ccbd606c2caa0a21cd625607944309a93d0c
nikhilnair23 <nikhilnair238@gmail.com>
2020-12-11 11:27:35 -0500

    Added some comments

--- app/src/main/java/com/example/allears/StatsActivity.java
+++ app/src/main/java/com/example/allears/StatsActivity.java
@@ -1,6 +1,5 @@
 package com.example.allears;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,6 +21,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class StatsActivity extends AppCompatActivity {
@@ -38,30 +39,36 @@ public class StatsActivity extends AppCompatActivity {
     private ArrayAdapter<String> trainingSpinnerAdapter;
     private ArrayAdapter<String> difficultySpinnerAdapter;
 
+    // Constants
     private static final String INTERVAL_TRAINING = "Interval";
     private static final String CHORD_TRAINING = "Chord";
     private static final String EASY_DIFFICULTY = "easy";
     private static final String MEDIUM_DIFFICULTY = "medium";
     private static final String HARD_DIFFICULTY = "hard";
 
-    final Context context = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        // setting database referencee to the stats table
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Stats");
         createItemList();
         createRecyclerView();
+
+        // default values
         trainingType = INTERVAL_TRAINING;
         difficulty = EASY_DIFFICULTY;
+
         trainingSpinner = findViewById(R.id.training_spinner);
         difficultySpinner = findViewById(R.id.difficulty_spinner);
+        // Fetch scores
         getScores(trainingType, difficulty);
         populateSpinners();
         setSpinnerListeners();
     }
 
+    // Getting the scores from firebase for a particular trainingType and difficulty
     private void getScores(String trainingType, String difficulty){
         scoreRef = mDatabase.child(trainingType).child(difficulty);
         scoreRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -89,10 +96,20 @@ public class StatsActivity extends AppCompatActivity {
         itemList = new ArrayList<>();
     }
 
-    //TODO: Sort the list as you add or maybe sort it at the end.
+    // Adds an item to the list and then sorts it
     public void addItem(Score score){
         itemList.add(score);
+        Collections.sort(itemList, new CustomComparator());
         rAdapter.notifyDataSetChanged();
+    }
+
+    // Custom comparator to sort the itemlist in descending order
+    public class CustomComparator implements Comparator<Score> {
+
+        @Override
+        public int compare(Score o1, Score o2) {
+            return o2.getScore().compareTo(o1.getScore());
+        }
     }
 
     public void createRecyclerView() {
@@ -112,8 +129,9 @@ public class StatsActivity extends AppCompatActivity {
         }
     }
 
+    // Adding dropdown values for training and difficult spinners
     private void populateSpinners(){
-        List<String> trainingList = new ArrayList<String>(
+        List<String> trainingList = new ArrayList<>(
                 Arrays.asList(INTERVAL_TRAINING, CHORD_TRAINING));
         List<String> difficultyList = new ArrayList<>(
                 Arrays.asList(EASY_DIFFICULTY, MEDIUM_DIFFICULTY, HARD_DIFFICULTY));
